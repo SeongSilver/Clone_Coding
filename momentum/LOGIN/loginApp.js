989f359387e54cf77090041c2bef830dcde68ef5
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-08-09 17:07:57 +0900

    로그인 작업 끝

--- momentum/LOGIN/loginApp.js
+++ momentum/LOGIN/loginApp.js
@@ -14,19 +14,68 @@ const loginButton = document.querySelector("#login-form button");
 
 // loginButton.addEventListener("click", onLoginBtnClick);
 
-function onLoginSubmit(tomato){
+// const link = document.querySelector("a");
+
+// function handleLinkClick(event){
+//     event.preventDefault();
+//     console.log(event);
+// }
+
+// link.addEventListener("click", handleLinkClick);
+
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME  = "hidden";
+const USERNAME_KEY ="username";
+const logout = document.querySelector("#logout");
+
+
+function onLoginSubmit(event){
     //preventDefault() : 어떤 이벤트의 기본행동이 일어나지 않게 막는 것 (form에서 button을 클릭시 submit되지 않게 막는 것 등)
-    tomato.preventDefault();
-    console.log(tomato);
+    event.preventDefault(); 
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    //===========localStorage 는 브라우저 내에 Application 하위 탭으로 해당 페이지에 정보를 저장(setItem), 불러오기(getItem), 삭제(removeItem) 등을 할 수 있다.
+    //===========localStrage setItem(key, value)형식으로 key는 string 형식으로 저장
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    // greeting.innerText = "안녕하세요 " + username + "님";
+    //===========``(백틱) 안에 String을 사용하면 띄어쓰기도 반영하며 변수 사용시 ${변수}와 같이 사용가능=============
+    paintGreetings();
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
-const link = document.querySelector("a");
-
-function handleLinkClick(event){
-    event.preventDefault();
-    console.log(event);
+function paintGreetings(){
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `안녕하세요 ${username} 님`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    logout.classList.remove(HIDDEN_CLASSNAME);
 }
 
-link.addEventListener("click", handleLinkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+// function addLogoutBtn(){
+//     var setBtn = document.createElement('input');
+
+//     setBtn.setAttribute("type", "button");
+//     setBtn.setAttribute("class", "logoutBtn");
+//     setBtn.setAttribute("value", "로그아웃");
+
+//     greeting.appendChild(setBtn);
+// }
+function clickLogout(event){
+    window.localStorage.removeItem(USERNAME_KEY);
+    window.location.reload();
+}
+
+logout.addEventListener("click", clickLogout);
+
+if(savedUsername === null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+    greeting.innerText ="<button>로그아웃</button>"
+}else{
+    //show the greeting
+    paintGreetings();
+}
+
