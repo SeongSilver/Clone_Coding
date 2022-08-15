0acbe4d06a3577a98ef91476020d2f5e26d2a35e
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-08-15 17:18:36 +0900

    canvas drawing 까지

--- momentum/ME-MAKER/app.js
+++ momentum/ME-MAKER/app.js
@@ -1,10 +1,13 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
 
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 
-ctx.lineWidth = 4;
+// ctx.lineWidth = 2;
+
 //fillStyle , strokeStyle 은 fillRect() / strokeRect() 코드 전에 와야 사용할 수 있다.
 //fillRect context 의 요소로서 fillRect(시작하는 x좌표, 시작하는 y좌표, 가로길이, 세로길이)
 //로 표현하며 해당 사각형을 가득 칠함
@@ -57,15 +60,131 @@ ctx.lineWidth = 4;
 // ctx.arc(250, 170, 20, 0, Math.PI);
 // ctx.fill();
 
-//2.0 Painting Lines
-const colors = ["##55efc4", "#81ecec", "#74b9ff", "#a29bfe", "#ffeaa7", "#fab1a0", "#ff7675"];
+//★★★★★★★★★★★★★2.0 Painting Lines
+// const colors = ["##55efc4", "#81ecec", "#74b9ff", "#a29bfe", "#ffeaa7", "#fab1a0", "#ff7675"];
 
-function onClick(event){
-    ctx.beginPath();
-    ctx.moveTo(0,0);
-    const color = colors[Math.floor(Math.random()*colors.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+// function onClick(event){
+//     ctx.beginPath();
+//     ctx.moveTo(0,0);
+//     const color = colors[Math.floor(Math.random()*colors.length)];
+//     ctx.strokeStyle = color;
+//     ctx.lineTo(event.offsetX, event.offsetY);
+//     ctx.stroke();
+// }
+// canvas.addEventListener("mousemove", onClick);
+
+//★★★★★★★★★★★★★2.2 Line Width
+//line-width의 id를 가진 input을 가져와서 그 값을 ctx 컨텍스트의 선두께값으로 가져온다
+const lineWidth = document.querySelector("#line-width");
+ctx.lineWidth = lineWidth.value;
+//선 두께 변환이라는 함수를 만들고 이 함수에서 일어나는 이벤트의 타겟의 값을 선두께로 잡아줘서
+//선두께가 변할 때마다 그 선두께 값을 컨텍스트의 선두께 값으로 업데이트 해준다
+//onMove함수에 beginPath()를 넣어주고 스트로크가 끝날 때마다 해당 패스를 끊어줌
+function onLineWidthChanging(event){
+    ctx.lineWidth = event.target.value;
 }
-canvas.addEventListener("mousemove", onClick);
+
+lineWidth.addEventListener("change", onLineWidthChanging);
+
+//★★★★★★★★★★★★★2.3 PaintColor-1
+const color = document.querySelector("#color");
+
+function onColorChange(event){
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
+color.addEventListener("change", onColorChange);
+
+//★★★★★★★★★★★★★2.4 PaintColor -2
+//document.getElement로 가져온 HTML객체는 자바스크립트에서 사용할 수 없기 때문에 
+//해당 클래스의 요소가 많아도 forEach문으로 돌릴 수 없어서 Array.from으로 해당 요소들을 colorOptions 배열에 담아준다
+
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
+
+function onColorClick(event){
+    //html태그 안에 'data-'로 담아놓은 객체는 매개변수.target안에 dataset.(-뒤에 담아놓은 이름)으로 사용가능
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    //type = color인 input박스의 컬러를 선택된 컬러의 새상으로 바꿔주는 기능
+    color.value = colorValue;
+}
+//color-option클래스의 요소가 여러개이기 때문에 각각 요소마다 같은 함수를 실행 시키기 위해
+//같은 배열에 묶고 그 배열에 forEach배열로 해당 요소 => 해당요소.이벤트추가(클릭, 함수)와 같이 기능을 삽입
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+
+
+//★★★★★★★★★★★★★2.5 Filling Mode
+const modeBtn = document.querySelector("#mode-btn");
+let isFilling = false;
+
+function onModeClick(){
+    if(isFilling){
+        isFilling = false;
+        modeBtn.innerText = "채우기";
+    }else{
+        isFilling = true;
+        modeBtn.innerText = "그리기";
+    }
+}
+
+modeBtn.addEventListener("click", onModeClick);
+
+function onCanvasClick(){
+    if(isFilling){
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
+
+//★★★★★★★★★★★★★2.6 Eraser
+//destroy버튼에 클릭 이벤트를 만들어주고 전체 지우기이기 떄문에 
+//fillStyle을 white로, fillRect(0, 0, 캔버스 길이, 캔버스 높이)로 그려준다
+const destroyBtn = document.querySelector("#destroy-btn");
+
+function onDestroyClick(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+destroyBtn.addEventListener("click", onDestroyClick);
+
+//eraseBtn에 strokeStyle을 흰색으로 주고 erase버튼을 클릭하면 그리기 버튼을 채우기 버튼으로 바꿔준다
+const eraseBtn = document.querySelector("#erase-btn");
+
+function onEraseClick(){
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "채우기";
+}
+
+eraseBtn.addEventListener("click", onEraseClick);
+
+//★★★★★★★★★★★★★2.1 Mouse Painting
+//mouse이벤트를 통해 isPainting을 변수로 두어 평상시는 false이지만 마우스가 눌려있을 때만 true로 두어
+//onMove함수에서 isPainting이 true일 때는 lineTo메서드를 이용해 현재 마우스의 좌표를 따라 선을 그리게 한다
+let isPainting = false;
+
+function onMove(event){
+    if(isPainting){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.beginPath();
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting(){
+    isPainting = true;
+}
+function cancelPainting(){
+    isPainting = false;
+}
+
+//mousemove : 마우스를 움직일 때 / mousedown : 마우스가 눌렸을 때
+//mouseup : 마우스가 눌린상태에서 안눌리게 될 때 / mouseleave : 마우스가 해당 영역을 떠났을 때
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("click", onCanvasClick);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
