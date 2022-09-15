96582c4eba69433ef6412b180a2ab97281c9bd3c
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-09-15 16:58:38 +0900

    유데미 CSS 컴포넌트화

--- react-for-beginners/src/components/ExpenseItem.js
+++ react-for-beginners/src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
-import './CSS/ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+import './ExpenseItem.css';
 
-function ExpenseItem(props){
+function ExpenseItem(props) {
 
-    return(
+    return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{props.id}. {props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
