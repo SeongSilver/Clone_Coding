fcda68eb3a8e00c5efc2bde24c355e43fac02ebb
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-10-07 23:57:10 +0900

    CSS 안먹는 오류 수정

--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -6,17 +6,17 @@ function Movie({ id, key, title, original_title, poster_path, genres }) {
 
     const image_url = `https://image.tmdb.org/t/p/w400/`;
 
+    console.log(genres);
+
     return (
         <div key={key} className="movie_card">
             <div className="card">
-                <Link to={`/movie/${id}`}><img className="movie_poster" src={image_url + poster_path} alt="no image"/></Link>
+                <Link to={`/movie/${id}`}><img className="movie_poster" src={image_url + poster_path} alt="no image" /></Link>
                 <h3 className="movie_title">{title}({original_title})</h3>
             </div>
         </div >
     )
 }
-
-
 
 
 export default Movie;
