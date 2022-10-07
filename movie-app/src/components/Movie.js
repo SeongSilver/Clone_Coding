ab54888d2ee858a96425add366ce601ad69abcc0
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-10-08 00:34:35 +0900

    rotate this 좀 제발

--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -2,14 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './CSS/Movie.css';
 
-function Movie({ id, key, title, original_title, poster_path, genres }) {
+function Movie({ id, key, title, original_title, poster_path }) {
 
     const image_url = `https://image.tmdb.org/t/p/w400/`;
 
-    console.log(genres);
+    const checkThis = () => {
+        const cardRotate = document.querySelector('.cardRotate');
+        cardRotate.classList.add("backRotate");
+        cardRotate.classList.remove("cardRotate");
+        console.log("action")
+    }
+    const checkOut = () => {
+        const cardRotate = document.querySelector('.cardRotate');
+        this.classList.add("cardRotate");
+        this.classList.remove("backRotate");
+    }
+    const rotate = () => {
+
+    }
 
     return (
-        <div key={key} className="movie_card">
+        <div key={key} className="movie_card cardRotate" onMouseOver={checkThis} onMouseLeave={checkOut}>
             <div className="card">
                 <Link to={`/movie/${id}`}><img className="movie_poster" src={image_url + poster_path} alt="no image" /></Link>
                 <h3 className="movie_title">{title}({original_title})</h3>
