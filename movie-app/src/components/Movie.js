b6e5333ed2ac6ade8b65db0585071a02e117ab79
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-10-08 21:33:54 +0900

    영화 그만

--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -6,23 +6,10 @@ function Movie({ id, key, title, original_title, poster_path }) {
 
     const image_url = `https://image.tmdb.org/t/p/w400/`;
 
-    const checkThis = () => {
-        const cardRotate = document.querySelector('.cardRotate');
-        cardRotate.classList.add("backRotate");
-        cardRotate.classList.remove("cardRotate");
-        console.log("action")
-    }
-    const checkOut = () => {
-        const cardRotate = document.querySelector('.cardRotate');
-        this.classList.add("cardRotate");
-        this.classList.remove("backRotate");
-    }
-    const rotate = () => {
 
-    }
 
     return (
-        <div key={key} className="movie_card cardRotate" onMouseOver={checkThis} onMouseLeave={checkOut}>
+        <div key={key} className="movie_card cardRotate">
             <div className="card">
                 <Link to={`/movie/${id}`}><img className="movie_poster" src={image_url + poster_path} alt="no image" /></Link>
                 <h3 className="movie_title">{title}({original_title})</h3>
