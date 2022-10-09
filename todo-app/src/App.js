40da0df67e6bb10a99c2ba68216ae60d3df4f35d
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-10-10 00:59:05 +0900

    일정관리 웹 end

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -21,10 +21,42 @@ function App() {
       checked: false,
     },
   ])
+  //고유값으로 사용될 id
+  //ref를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; //nextId 1씩 더하기
+    }, [todos]
+  )
+
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id));
+    }, [todos]
+  )
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        //특정 id를 가지고 있는 객체의 checked값을 반전시킨다 
+        //불변성을 유지하면서 특정 배열 원소를 업데이트해야 할 때 이렇게 map을 사용
+        todos.map(todo => todo.id === id ? { ...todo, checked: !todo.checked } : todo)
+      )
+    }, [todos]
+  )
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList todos={todos} />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 }
