0e6408de927d4403c5ea4a477f490c9e3898ee3e
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-10-11 01:24:47 +0900

    최적화 React.memo, useReducer

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,57 +1,87 @@
-import { useState, useRef, useCallback } from 'react';
+import { useReducer, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
       checked: false,
-    },
-  ])
+    })
+  }
+  return array;
+}
+
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      // { type: 'INSERT', todo: { id: 1, text: 'todo', checked: false } }
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      // {type: 'REOMOVE', id:1}
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE':
+      // {type:'Remove', id:1}
+      return todos.map(todo => todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,);
+    default: return todos;
+  }
+}
+function App() {
+  // const [todos, setTodos] = useState(createBulkTodos)
   //고유값으로 사용될 id
   //ref를 사용하여 변수 담기
-  const nextId = useRef(4);
+  // const nextId = useRef(2501);
 
-  const onInsert = useCallback(
-    text => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1; //nextId 1씩 더하기
-    }, [todos]
-  )
+  // const onInsert = useCallback(
+  //   text => {
+  //     const todo = {
+  //       id: nextId.current,
+  //       text,
+  //       checked: false,
+  //     };
+  //     setTodos(todos => todos.concat(todo));
+  //     nextId.current += 1; //nextId 1씩 더하기
+  //   }, []
+  // )
 
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos.filter(todo => todo.id !== id));
-    }, [todos]
-  )
+  // const onRemove = useCallback(
+  //   id => {
+  //     setTodos(todos => todos.filter(todo => todo.id !== id));
+  //   }, []
+  // )
 
-  const onToggle = useCallback(
-    id => {
-      setTodos(
-        //특정 id를 가지고 있는 객체의 checked값을 반전시킨다 
-        //불변성을 유지하면서 특정 배열 원소를 업데이트해야 할 때 이렇게 map을 사용
-        todos.map(todo => todo.id === id ? { ...todo, checked: !todo.checked } : todo)
-      )
-    }, [todos]
-  )
+  // const onToggle = useCallback(
+  //   id => {
+  //     setTodos(todos =>
+  //       //특정 id를 가지고 있는 객체의 checked값을 반전시킨다 
+  //       //불변성을 유지하면서 특정 배열 원소를 업데이트해야 할 때 이렇게 map을 사용
+  //       todos.map(todo => todo.id === id ? { ...todo, checked: !todo.checked } : todo)
+  //     )
+  //   }, []
+  // )
+  //==========**useReducer 사용하기
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  const nextId = useRef(2501);
+  const onInsert = useCallback(text => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    dispatch({ type: 'INSERT', todo });
+    nextId.current += 1; //nextId 1씩 더하기
+  }, []);
+
+  const onRemove = useCallback(id => {
+    dispatch({ type: 'REMOVE', id });
+  }, []);
+
+  const onToggle = useCallback(id => {
+    dispatch({ type: 'TOGGLE', id });
+  }, [])
 
   return (
     <TodoTemplate>
--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.css';
 
@@ -16,4 +17,9 @@ const TodoList = ({ todos, onRemove, onToggle }) => {
     )
 }
 
-export default TodoList;
+//React.memo를 사용한 최적화 코드는 현재 플젝 성능에 전혀 영향 X
+//TodoList 컴포넌트의 부모 컴포넌트인App컴포넌트가 리렌더링되는 유일한 이유가 todos배열이 업데이트 될 때이기 때문
+
+//App컴포넌트에 다른 state가 추가되어 해당 값들이 업데이트 될 때는 TodoList 컴포넌트가 
+//불필요한 렌더링을 할 수도 잇기때문에 React.memo로 최적화
+export default React.memo(TodoList);
