8d6a61d509cc77128dddfb310e5fb1fe179dd4cf
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-10-15 16:33:53 +0900

    tailwind css 오류 잡기 babelrc 삭제, postcss.config.js autoprefixer 삭제

--- next-portfolio/pages/index.js
+++ next-portfolio/pages/index.js
@@ -1,15 +1,19 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout>
       <Head>
         <title>윈스턴's 포트폴리오</title>
         <meta name="description" content="오늘도 리액트" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>윈스턴의 빡코딩</h1>
-    </div>
+
+      <h1 className="text-3xl font-bold underline">
+        홈입니다
+      </h1>
+
+    </Layout>
   )
 }
