b339118beb195a134dc5f9e5ebff8fb76ea3d387
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-10-17 00:05:04 +0900

    애니메이션, Link, 다크모드중 다크모드 svg property 안먹는 현상 해결해야됨

--- next-portfolio/pages/projects.js
+++ next-portfolio/pages/projects.js
@@ -1,8 +1,14 @@
 import Layout from '../components/layout'
+import Head from 'next/head'
 
 export default function Projects() {
     return (
         <Layout>
+            <Head>
+                <title>윈스턴's 포트폴리오</title>
+                <meta name="description" content="오늘도 리액트" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <h1>프로젝트</h1>
         </Layout>
     )
