d522be19adc9bef7e5108719c64139322af95b3e
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-10-16 21:40:05 +0900

    layout 작업 완

--- next-portfolio/pages/index.js
+++ next-portfolio/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
+import Hero from '../components/home/hero'
 
 export default function Home() {
   return (
@@ -9,11 +10,11 @@ export default function Home() {
         <meta name="description" content="오늘도 리액트" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1 className="text-3xl font-bold underline">
-        홈입니다
-      </h1>
-
+      <section className="flex min-h-screen  flex-col items-center justify-centertext-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <Hero />
+        </div>
+      </section>
     </Layout>
   )
 }
