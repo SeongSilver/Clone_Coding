44ecc78f9ae89f8bdb664fcb1c5ee484e8d16151
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-10-31 00:36:29 +0900

    mongoDB REST API, 요청 검증기능 등 추가

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -5,9 +5,13 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
+
+const post = new Router();
+//postsCtrl.checkObjectId로 검증이 필요한 부분에 post.ctrl.js에서 만든 checkObjectId 미들웨어 추가
+post.get('/', postsCtrl.read);
+post.delete('/', postsCtrl.remove);
+post.patch('/', postsCtrl.update);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
 export default posts;
