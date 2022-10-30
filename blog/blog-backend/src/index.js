038a3635282e0b049d9e79314ea31530d81afe41
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-10-30 21:27:16 +0900

    mongoDB 연동, esm 문법으로 import/export전환

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,39 +1,5 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
+//이 파일에서만 no-global-assign ESLint 옵션을 비활성화
+/* eslint-disable no-global-assign */
 
-const api = require('./api');
-
-const app = new Koa();
-const router = new Router();
-
-//========라우트 모듈화
-router.use('/api', api.routes()); //api라우트 적용
-
-//라우터 적용 전에 bodyParser적용
-app.use(bodyParser());
-
-
-// //라우터 설정
-// router.get('/', ctx => {
-//     ctx.body = '홈';
-// });
-
-// router.get('/about/:name?', ctx => {
-//     const { name } = ctx.params;
-//     //name존재 유무에 따라 다른 결과 출력
-//     ctx.body = name ? `${name}의소개` : '소개';
-// })
-
-// router.get('/posts', ctx => {
-//     const { id } = ctx.query;
-//     //id의존재 유무에 따라 다른 결과 출력
-//     ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
-// })
-
-//app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods());
-
-app.listen(4000, () => {
-    console.log('Listening to port 4000');
-})
+require = require('esm')(module /*, options */);
+module.exports = require('./main.js');
