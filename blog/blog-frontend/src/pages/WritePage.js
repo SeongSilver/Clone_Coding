ff9cef1267b00eacadec56830a0ede76b7ce5d60
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-11-04 01:14:36 +0900

    Editor, TagBox 컨테이너, 리덕스 연결 완료

--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Responsive from '../components/common/Responsive';
-import Editor from '../components/write/Editor';
-import TagBox from '../components/write/TagBox';
+import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
 import WriteActionButtons from '../components/write/WriteActionButtons';
 
 function WritePage() {
   return (
     <Responsive>
-      <Editor />
-      <TagBox />
+      <EditorContainer />
+      <TagBoxContainer />
       <WriteActionButtons />
     </Responsive>
   );
