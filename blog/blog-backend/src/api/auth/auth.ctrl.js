1e99f4076be5a73a4935f25ae246b2062d1abdd8
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-11-26 17:14:26 +0900

    auth.ctrl수정

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -8,45 +8,45 @@ import User from '../../models/users';
         password:'mypass123;
     }
 */
-export const register = async ctx => {
-    //Request Body 검증하기
-    const schema = Joi.object().keys({
-        username: Joi.string().alphanum().min(3).max(20).required(),
-        password: Joi.string().required()
+export const register = async (ctx) => {
+  //Request Body 검증하기
+  const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string().required(),
+  });
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  const { username, password } = ctx.request.body;
+  try {
+    //username이 이미 존재하는지 확인
+    const exists = await User.findByUsername(username);
+    if (exists) {
+      ctx.status = 409; //Conflict 충돌
+      return;
+    }
+
+    const user = new User({
+      username,
     });
-    const result = schema.validate(ctx.request.body);
-    if (result.error) {
-        ctx.status = 400;
-        ctx.body = result.error
-        return;
-    }
+    await user.setPassword(password); //비밀번호 설정
+    await user.save(); //DB저장
+    //응답할 데이터에서 hashedPasswor필드 제거
+    ctx.body = user.serialize();
 
-    const { username, password } = ctx.request.body;
-    try {
-        //username이 이미 존재하는지 확인
-        const exists = await User.findByUsername(username);
-        if (exists) {
-            ctx.status = 409; //Conflict 충돌
-            return;
-        }
-
-        const user = new User({
-            username,
-        });
-        await user.setPassword(password); //비밀번호 설정
-        await user.save();  //DB저장
-        //응답할 데이터에서 hashedPasswor필드 제거
-        ctx.body = user.serialize();
-
-        const token = user.generateToken();
-        ctx.cookies.set('access_token', token, {
-            maxAge: 1000 * 60 * 60 * 24 * 7, //7일
-            httpOnly: true,
-        });
-    } catch (e) {
-        ctx.throw(500, e);
-    }
-}
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, //7일
+      httpOnly: true,
+    });
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 /*
     POST /api/auth/login
@@ -55,57 +55,57 @@ export const register = async ctx => {
         password:'mypass123'
     }
 */
-export const login = async ctx => {
-    const { username, password } = ctx.request.body;
+export const login = async (ctx) => {
+  const { username, password } = ctx.request.body;
 
-    //username,password없으면 에러처리
-    if (!username || !password) {
-        ctx.status = 401; //Unauthorized
-        return;
+  //username,password없으면 에러처리
+  if (!username || !password) {
+    ctx.status = 401; //Unauthorized
+    return;
+  }
+
+  try {
+    const user = await User.findByUsername(username);
+    //계정이 존재하지 않으면 에러처리
+    if (!user) {
+      ctx.status = 401;
+      return;
     }
-
-    try {
-        const user = await User.findByUsername(username);
-        //계정이 존재하지 않으면 에러처리
-        if (!user) {
-            ctx.status = 401;
-            return;
-        }
-        const valid = await user.checkPassword(password);
-        //잘못된 비밀번호
-        if (!valid) {
-            ctx.status = 401;
-            return;
-        }
-        ctx.body = user.serialize();
-
-        const token = user.generateToken();
-        ctx.cookies.set('access_token', token, {
-            maxAge: 1000 * 60 * 60 * 24 * 7, //7일
-            httpOnly: true,
-        });
-    } catch (e) {
-        ctx.throw(500, e);
+    const valid = await user.checkPassword(password);
+    //잘못된 비밀번호
+    if (!valid) {
+      ctx.status = 401;
+      return;
     }
-}
+    ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, //7일
+      httpOnly: true,
+    });
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 /*
     GET /api/auth/check
 */
-export const check = async ctx => {
-    const { user } = ctx.state;
-    if (!user) {
-        //로그인중 아님
-        ctx.status = 401; //Unauthorized
-        return;
-    }
-    ctx.body = user;
-}
+export const check = async (ctx) => {
+  const { user } = ctx.state;
+  if (!user) {
+    //로그인중 아님
+    ctx.status = 401; //Unauthorized
+    return;
+  }
+  ctx.body = user;
+};
 
 /*
     POST /api/auth/logout
 */
-export const logout = async ctx => {
-    ctx.cookies.set('access_token');
-    ctx.status = 204; //No Content
-}
+export const logout = async (ctx) => {
+  ctx.cookies.set('access_token');
+  ctx.status = 204; //No Content
+};
