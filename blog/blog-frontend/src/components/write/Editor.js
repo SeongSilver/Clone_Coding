3465465f97a91744636b639c853ac451202e2fdb
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-11-27 23:55:03 +0900

    write_Editor.js 수정

--- blog/blog-frontend/src/components/write/Editor.js
+++ blog/blog-frontend/src/components/write/Editor.js
@@ -6,7 +6,7 @@ import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
 
 const EditorBlock = styled(Responsive)`
-  //페이지 위아래 여백 지정
+  /*페이지 위아래 여백 지정 */
   padding-top: 5rem;
   padding-bottom: 5rem;
 `;
@@ -22,7 +22,7 @@ const TitleInput = styled.input`
 `;
 
 const QuillWrapper = styled.div`
-  //최소 크기 지정 및 padding제거
+  /*최소 크기 지정 및 padding제거 */
   .ql-editor {
     padding: 0;
     min-height: 320px;
@@ -34,17 +34,17 @@ const QuillWrapper = styled.div`
   }
 `;
 
-const Editor = ({ title, body, onChangeField }) => {
-  const quillElement = useRef(null); //Quill을 적용할 DivElement설정
-  const quillInstance = useRef(null); //Quill 인스턴스 설정
+const Editor = () => {
+  const quillElement = useRef(null);
+  const quillInstance = useRef(null);
 
   useEffect(() => {
     quillInstance.current = new Quill(quillElement.current, {
       theme: 'bubble',
       placeholder: '내용을 작성하세요...',
       modules: {
-        //더많은옵션
-        //https://quilljs.com/docs/modules/toolbar/ 참고
+        // 더 많은 옵션
+        // https://quilljs.com/docs/modules/toolbar/ 참고
         toolbar: [
           [{ header: '1' }, { header: '2' }],
           ['bold', 'italic', 'underline', 'strike'],
@@ -53,26 +53,11 @@ const Editor = ({ title, body, onChangeField }) => {
         ],
       },
     });
-
-    const quill = quillInstance.current;
-    quill.on('text-change', (delta, oldDelta, source) => {
-      if (source === 'user') {
-        onChangeField({ key: 'body', value: quill.root.innerHTML });
-      }
-    });
-  }, [onChangeField]);
-
-  const onChangeTitle = (e) => {
-    onChangeField({ key: 'title', value: e.target.value });
-  };
+  }, []);
 
   return (
     <EditorBlock>
-      <TitleInput
-        placeholder="제목을 입력하세요"
-        onChange={onChangeTitle}
-        title={title}
-      />
+      <TitleInput placeholder="제목을 입력하세요" />
       <QuillWrapper>
         <div ref={quillElement} />
       </QuillWrapper>
